8233935a281cc635c97b0a00186c57d9e9b01ce0
H7KZ <74021016+H7KZ@users.noreply.github.com>
2023-11-04 15:25:13 +0100

    hjgkh

--- src/supabase.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabase = createClient('https://htlmdhpmeannusdqtfyf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh0bG1kaHBtZWFubnVzZHF0ZnlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg5OTYxNjUsImV4cCI6MjAxNDU3MjE2NX0.PL0gW2FL0ctpq-W8VfE-MJ-OQimYXLIRCL_IT_IwizY');
-
-export default supabase;
